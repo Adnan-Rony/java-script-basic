efde4739a660e15fc380c03c59875540dd58580e
Adnan-Rony <adnanrony19@gmail.com>
2023-07-28 23:08:53 +0600

    variable basic concept

---
+++ variable.js
@@ -0,0 +1,18 @@
+//numeric
+var price = 40;
+var age = 20;
+var temperature = 38;
+var weight =54;
+var cgpa = 3;
+
+//string
+var person= "adnan rony" ;
+var location ="daffodil campus";
+var special ='web devolopment';
+
+// boolean
+var serious =true;
+var assigment=true;
+var issingle=false;
+
+
