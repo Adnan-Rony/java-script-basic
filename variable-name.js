0117d35aa0b59b2352f89878678d62194eac4eee
Adnan-Rony <adnanrony19@gmail.com>
2023-07-28 23:20:41 +0600

    variable name

---
+++ variable-name.js
@@ -0,0 +1,7 @@
+//important question of interview.....
+
+// variable name can not be any keywords..
+// variable name has to be one word.no space..
+//variable name can not have quatation..
+//variable name can not start a number but can ends with number..
+//long variable used(underscored) &(camel case**)
