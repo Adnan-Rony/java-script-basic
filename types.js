1c83601ba9721f4dd8f4cfb65af5b5a3a67fb177
Adnan-Rony <adnanrony19@gmail.com>
2023-07-29 11:50:39 +0600

    types

--- types.js
+++ types.js
@@ -1,8 +1,28 @@
 var price=100;
 // which types of variable
 //console.log(typeof price);
+
+
 var price='200';
 //console.log(typeof price);
 
 var isHappy=true;
-console.log(typeof isHappy);
+//console.log(typeof isHappy);
+
+
+var romantic;
+// console.log(romantic);
+
+
+// flote problem
+var num1=0.1;
+var num2=0.2;
+var sum=num1+num2;
+// tofixed used korle string type hoye jay..
+sum=sum.toFixed(2);
+// string to float convert 
+sum2=parseFloat(sum);
+console.log(sum2);
+
+
+
