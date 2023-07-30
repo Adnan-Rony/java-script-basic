fb5bfcbfc63bdb933238b2732037eff07a1c63d1
Adnan-Rony <adnanrony19@gmail.com>
2023-07-30 23:40:41 +0600

    loop

---
+++ loop/while.js
@@ -0,0 +1,15 @@
+// var roastGiven=0;
+// while(roastGiven<7)
+// {
+//     console.log('roast deb vai!');
+//     roastGiven++;
+//     console.log(roastGiven);
+// }
+
+
+// numbers
+var numbers=0;
+while(numbers<20){
+    console.log(numbers);
+    numbers++;
+}
