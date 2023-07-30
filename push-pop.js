13db61ef14fa291754757980d2dae9c2c83067b8
Adnan-Rony <adnanrony19@gmail.com>
2023-07-30 10:20:17 +0600

    push pop of array

---
+++ push-pop.js
@@ -0,0 +1,43 @@
+// var numbers =[10,20,30,40];
+// numbers.push(65);
+
+// push menans add last elemets of array
+// console.log(numbers);
+// var friends=['adnan','rony','sami','adu'];
+// friends.push('aduyan');
+// friends.push('salam');
+// console.log(friends);
+
+
+// pop means ber kore deuya last elements
+var numbers =[10,20,30,40];
+var friends=['adnan','rony','sami','adu'];
+friends.pop();
+numbers.pop();
+// console.log(numbers);
+// console.log(friends);
+
+
+var numbers =[10,20,30,40];
+// var friends=['adnan','rony','sami','adu'];
+// friends.pop();
+var elements=numbers.pop();
+// numbers.pop();
+// console.log(elements);
+
+
+
+// push menans add first elemets of array{unshift}
+
+var friends=['adnan','rony','sami','adu'];
+friends.unshift('aduyan');
+// console.log(friends);
+
+var numbers =[10,20,30,40];
+numbers.unshift(100);
+// console.log(numbers);
+
+
+
+
+//
