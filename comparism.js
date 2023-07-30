4b1a85c99b3c63c6b37881e3ef7ba71b5b5a79d7
Adnan-Rony <adnanrony19@gmail.com>
2023-07-30 18:16:35 +0600

    comparism method

---
+++ comparism.js
@@ -0,0 +1,15 @@
+// console.log(5<6);true
+// console.log(5>6);false
+// console.log(5==6);false
+// console.log(5==5);true
+// console.log(5!=6);
+// console.log(15!= 15);
+// console.log(5<=6);
+// console.log(6<=6);
+// console.log(16<=6);
+// console.log(5>=6);
+
+var num1=56;
+var num2=59;
+
+console.log(num1 < num2);
