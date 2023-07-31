22a5c2598defcbf346f30524a6811214e4fa5eea
Adnan-Rony <adnanrony19@gmail.com>
2023-07-31 14:31:00 +0600

    practice

---
+++ loop/practice-problem.js
@@ -0,0 +1,61 @@
+//1
+// var i=0;
+// for(i=0;i<=39;i++)
+// {
+//     console.log('ajke amar mon vala nei!');
+// }
+
+
+
+// 2 number
+// for(var i=58;i<=98;i++)
+// {
+//     console.log(i);
+// }
+
+
+// 3 number
+// for(var i=412;i<=456;i+=2)
+// {
+//     console.log(i);
+// }
+
+
+//4 number
+// for(var i=581;i<=623;i+=2)
+// {
+//     console.log(i);
+// }
+
+
+// 5 number
+// var topics=['html','css','pera','tailwind','daisyui','boatstrap','array','loop'];
+
+// for(var i=0;i<topics.length;i++)
+// {
+//     var topicOutput=topics[i];
+//     console.log(topicOutput);
+// }
+
+
+// 6 number
+// var topics=['html','css','html5','tailwind','daisyui','boatstrap','array','loop'];
+// var i=0;
+// while(i<=topics.length)
+// {
+//     var topicOutput=topics[i];
+//     console.log(topicOutput);
+//     i++;
+// }
+
+
+// 7 number
+for(var i=30;i<=86;i++)
+{
+    
+    if(i>=50)
+    {
+        break;
+    }
+    console.log(i);
+}
