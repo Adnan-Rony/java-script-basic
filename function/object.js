8c5371b947c5cf0272d9c128e48cb6d42dec4bd1
Adnan-Rony <adnanrony19@gmail.com>
2023-08-02 01:46:58 +0600

    object of function

---
+++ function/object.js
@@ -0,0 +1,31 @@
+// basic formula of all property show
+var myConputer ={
+    brand: 'HP',
+    price:60000,
+    color: 'black'
+}
+//console.log(myConputer);
+
+
+
+// only one property show in display
+var myConputer ={
+    brand: 'HP',
+    price:60000,
+    color: 'black'
+}
+// console.log(myConputer.brand); //only brand property show
+// console.log(myConputer.price); //only price property show
+
+
+
+// after property value change
+var myConputer ={
+    brand: 'HP',
+    price:60000,
+    color: 'black'
+}
+myConputer.color='red';
+console.log(myConputer.color);
+
+
