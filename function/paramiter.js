7d462c153a578448b8b781ffc930300ed0f29ffa
Adnan-Rony <adnanrony19@gmail.com>
2023-08-02 01:12:46 +0600

    return of function

---
+++ function/paramiter.js
@@ -0,0 +1,36 @@
+function bringSingara(money){
+    console.log(money,' taka disen?');
+    console.log('mama singara den');
+  
+}
+// bringSingara(100);
+
+
+// var taka=200
+// bringSingara(taka);
+
+// new
+function add(num1,num2)
+{
+    var adding=num1+num2;
+    console.log('adding the value');
+console.log(num1,num2);
+
+}
+// add(12,15);
+
+
+
+// new
+function sum(a,b,c,d,e){
+    console.log('adding the value');
+    var sum=a+b+c+d+e;
+    console.log(sum);
+}
+sum(1,2,3,4,5);
+
+
+
+
+
+
---
+++ function/return.js
@@ -0,0 +1,19 @@
+// function add(num1,num2){
+//     console.log(num1,num2);
+//     var sum=num1+num2;
+//     // console.log()
+//     return sum;
+// }
+// var total=add(10,20);
+// console.log('total',total);
+
+
+// new
+function BringSingara(money){
+    var singaraPrice=10;
+    var quantity=money/ singaraPrice;
+    return quantity;
+}
+var mytaka=200;
+var singara=BringSingara(mytaka);
+console.log('eating singara',singara);
