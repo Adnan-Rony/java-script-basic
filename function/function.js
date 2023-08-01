3c754156001ff1ccdea126b55099b164af035787
Adnan-Rony <adnanrony19@gmail.com>
2023-08-01 22:11:24 +0600

    function js

---
+++ function/function.js
@@ -0,0 +1,23 @@
+function Order(){
+console.log('stand up');
+console.log('go to drawing room');
+console.log('turn on the switch');
+}
+
+Order();
+console.log('i want to best web devoloper');
+console.log('valo lage nah kissu');
+
+
+Order();
+console.log('next 6 months i wanna be attend regular class of ph');
+console.log('valo lage nah kissu');
+
+
+
+
+function next(){
+    console.log('i loveeeee you');
+    console.log('i loveeeee you tooooooooooo');
+}
+next();
