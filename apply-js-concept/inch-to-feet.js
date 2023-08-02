45eaa9fbfcb74b83dd8ca365dadae99bd2b17ee3
Adnan-Rony <adnanrony19@gmail.com>
2023-08-02 20:59:52 +0600

    converter

---
+++ apply-js-concept/inch-to-feet.js
@@ -0,0 +1,29 @@
+const MyInch=144;
+const MyFeet =MyInch/12;
+// console.log('feet is :',MyFeet);
+
+
+
+const MyInch2=148;
+const MyFeet2 =MyInch2/12;
+// const adu =MyFeet2.toFixed(2);
+// console.log('feet is :',MyFeet2);
+
+
+
+
+// function
+function convert(inch){
+    
+    const MyFeet=inch/12;
+    // console.log(MyFeet);
+    return MyFeet;
+}
+const person1=144;
+const person2=60;
+const person3=24;
+console.log('person1 convert',person1);
+console.log('person2 convert',person2);
+console.log('person3 convert',person3);
+
+
---
+++ apply-js-concept/mile-to-km.js
@@ -0,0 +1,12 @@
+function converter(miles)
+{
+    const km=miles*1.609;
+    return km;
+}
+const person1=5;
+const person1KmConvert1=converter(person1);
+console.log('convert result of mile to KM:',person1KmConvert1);
+
+const person2=10;
+const person1KmConvert2=converter(person1);
+console.log('convert result of mile to KM:',person1KmConvert2);
