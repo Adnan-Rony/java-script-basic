5e72c8288865ac0bfd781d90f6e9dc9d521ce7de
Adnan-Rony <adnanrony19@gmail.com>
2023-08-05 01:04:41 +0600

    math related oparation

---
+++ math/math.js
@@ -0,0 +1,51 @@
+const result=Math.pow(3,8);
+// console.log(result);
+
+
+// 
+const num1=20;
+const num2=45;
+const gap=Math.abs(num1-num2); //math.abs use korle boro theke soto count korbe i mean,absulate man print hobe;
+if(gap<5)
+{
+    // console.log('you guys are friend');
+}
+else{
+    // console.log('not friends');
+}
+
+// float number-1
+const number=2.50556;
+const fullNumber=Math.round(number);  //round korle(.5 hole akberbe)
+// console.log(fullNumber);
+
+// // float number-2
+const result2=Math.ceil(2.0000001);
+// console.log(result2);
+
+// 
+const result3=Math.floor(344.32828);
+// console.log(result3);
+
+
+// random number 0-1 moddhe.
+// console.log(Math.random());
+
+// random number,ami bole dile toto pojonto random number dite parbeeee
+const random=Math.random()*100;
+// console.log(random);
+
+const random2=Math.round(Math.random()*100); //round output print korte chaile
+// console.log(random2);
+
+
+// for loop used kore
+for(let i=0;i<20;i++)
+{
+    
+const random2=Math.round(Math.random()*6); 
+console.log(random2);
+
+}
+
+
