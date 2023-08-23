dde3e435b645d47182b507915921f24d7bc05ddf
Adnan-Rony <adnanrony19@gmail.com>
2023-08-23 23:46:18 +0600

    es6

---
+++ ES-6/var-let-const.js
@@ -0,0 +1,22 @@
+// const count = 2;
+// count = count + 5;
+// // console.log(count);
+
+
+let money = 2;
+money = money + 5;
+console.log(money);
+
+
+// 
+const numbers=[2,3,5,6,8,9,12,444]
+
+// const numbers=[2,3,4,5,6,7,99] not possible
+
+numbers[1]=44;
+
+numbers.push(5,6,7,88);
+
+console.log(numbers);
+
+
