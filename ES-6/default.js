fe785c1be007a4b32760f5b162ce0c12d3c445d2
Adnan-Rony <adnanrony19@gmail.com>
2023-08-23 23:55:14 +0600

    es6

---
+++ ES-6/default.js
@@ -0,0 +1,11 @@
+
+
+function add(num1,num2)
+{
+    const result =num1+num2;
+    console.log(num1,num2,result);
+    return result;
+}
+
+// const sum =add(2,4);
+const sum =add(4); //undifined and NaN
