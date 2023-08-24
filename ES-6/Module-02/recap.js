c89919cd8ae3a8b932958de2831bab81ac31d481
Adnan-Rony <adnanrony19@gmail.com>
2023-08-24 17:15:06 +0600

    es-6-2

---
+++ ES-6/Module-02/recap.js
@@ -0,0 +1,11 @@
+//spread
+
+const a=110;
+const arr=[12,23,56];
+const obj ={
+    name:'adnan rony',
+};
+
+const final =`mu name is ${obj.name} and i have some extra ${a} money and my pen price is ${arr[1]}`
+
+console.log(final);
